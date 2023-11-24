8b89b78fdf37d4f984aec5f1da6cbf3fc62f56b3
Dino Hensen <dino.hensen@gmail.com>
2023-11-24 09:42:06 +0100

    Add descriptions to harpoon keybindings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,18 +1,18 @@
 local mark = require "harpoon.mark"
 local ui = require "harpoon.ui"
 
-vim.keymap.set("n", "<C-e>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-e>a", mark.add_file, { desc = "harpoon: mark file" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "harpoon: toggle quick menu" })
 
 vim.keymap.set("n", "<C-e>1", function()
     ui.nav_file(1)
-end)
+end, { desc = "harpoon: go to 1" })
 vim.keymap.set("n", "<C-e>2", function()
     ui.nav_file(2)
-end)
+end, { desc = "harpoon: go to 2" })
 vim.keymap.set("n", "<C-e>3", function()
     ui.nav_file(3)
-end)
+end, { desc = "harpoon: go to 3" })
 vim.keymap.set("n", "<C-e>n", function()
     ui.nav_next()
 end, { desc = "harpoon: go to next" })
