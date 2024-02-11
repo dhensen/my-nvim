a6c96f1e9db837325e8964d93be97f29cae7c70a
Dino Hensen <dino.hensen@gmail.com>
2024-02-11 22:24:10 +0100

    fix

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -119,7 +119,7 @@ return require("packer").startup(function(use)
     }
     use {
         "nvim-lualine/lualine.nvim",
-        requires = { "kyazdani42/nvim-web-devicons", opt = true },
+        requires = { "nvim-tree/nvim-web-devicons", opt = true },
     }
 
     use { "williamboman/mason.nvim" }
