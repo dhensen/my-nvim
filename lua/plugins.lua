1f2ea0de469371419a8f477e42769c08f2b640cf
Dino Hensen <dino.hensen@gmail.com>
2024-03-05 10:19:06 +0100

    add markdown-preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -210,6 +210,14 @@ return require("packer").startup(function(use)
     }
 
     use { "github/copilot.vim" }
+    use {
+        "iamcco/markdown-preview.nvim",
+        run = "cd app && npm install",
+        setup = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    }
 
     if packer_bootstrap then
         require("packer").sync()
