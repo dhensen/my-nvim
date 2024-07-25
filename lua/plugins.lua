eb6725bc1854e62495d798ebc329908415cf2922
Dino Hensen <dino.hensen@gmail.com>
2024-07-25 23:08:40 +0200

    Add nvim-nio for neotest

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -58,6 +58,7 @@ return require("packer").startup(function(use)
     use {
         "nvim-neotest/neotest",
         requires = {
+            "nvim-neotest/nvim-nio",
             "nvim-lua/plenary.nvim",
             "nvim-treesitter/nvim-treesitter",
             "antoinemadec/FixCursorHold.nvim",
