c5399e6b211d09096247a59a4335ce95c4fa22f8
Dino Hensen <dino.hensen@gmail.com>
2024-09-29 10:21:05 +0200

    when cuttign up old single file into multiple files, this treesitter config got lost

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,32 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update {with_sync = true}
-    end
+    build = ":TSUpdate",
+    config = function()
+        local configs = require "nvim-treesitter.configs"
+
+        configs.setup {
+            ensure_installed = {
+                "vimdoc",
+                "python",
+                "javascript",
+                "lua",
+                "typescript",
+                "go",
+                "rust",
+                "c",
+                "php",
+                "terraform",
+                "comment",
+            },
+            sync_install = false,
+            highlight = { enable = true, additional_vim_regex_highlighting = false },
+            indent = { enable = true },
+            -- context_commentstring nvim-treesitter module is deprecated, use require('ts_context_commentstring').setup {} and set vim.g.skip_ts_context_commentstring_module = true to speed up loading instead.
+            -- This feature will be removed in ts_context_commentstring version in the future (see https://github.com/JoosepAlviste/nvim-ts-context-commentstring/issues/82 for more info)
+            -- context_commentstring = {
+            --     enable = true,
+            --     enable_autocmd = false,
+            -- },
+        }
+    end,
 }
