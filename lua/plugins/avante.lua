7d850b66a8dbb40be91f5d9c2c99d94e9befc61d
Dino Hensen <dino.hensen@gmail.com>
2025-04-03 10:49:34 +0200

    use claude

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -3,7 +3,8 @@ return {
     event = "VeryLazy",
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-        provider = "copilot",
+        provider = "claude",
+        -- provider = "copilot",
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = "make",
