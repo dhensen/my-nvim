c4e017ef33a00d7e12dce7e6f97c3484803d6787
Dino Hensen <dino.hensen@gmail.com>
2025-07-21 07:45:15 +0200

    feat(nvim-notify): add custom configuration with static stages, bottom-up display, and timeout setting

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,1 +1,12 @@
-return { "rcarriga/nvim-notify" }
+return {
+    "rcarriga/nvim-notify",
+    config = function()
+        require("notify").setup {
+            stages = "static",
+            top_down = false,
+            timeout = 3000,
+        }
+
+        vim.notify = require "notify"
+    end,
+}
