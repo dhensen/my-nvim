efe5c762c2134d8c633bfb3397a183509bec21a6
Dino Hensen <dino.hensen@gmail.com>
2026-02-11 13:45:54 +0100

    change to maintained markdown-preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,6 +1,12 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    init = function() vim.g.mkdp_filetypes = {"markdown"} end,
-    ft = {"markdown"}
+    "selimacerbas/markdown-preview.nvim",
+    dependencies = { "selimacerbas/live-server.nvim" },
+    config = function()
+        require("markdown_preview").setup {
+            -- all optional; sane defaults shown
+            port = 8421,
+            open_browser = true,
+            debounce_ms = 300,
+        }
+    end,
 }
