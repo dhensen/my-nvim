7cb90581556d0a29261b9b46d9bc2b69405a336e
Dino Hensen <dino.hensen@gmail.com>
2026-06-20 00:27:26 +0200

    Update none-ls supports_method call

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -22,7 +22,7 @@ return {
             },
             temp_dir = tmp_dir,
             on_attach = function(client, bufnr)
-                if client.supports_method "textDocument/formatting" then
+                if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting, bufnr) then
                     vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
                     vim.api.nvim_create_autocmd("BufWritePre", {
                         group = augroup,
